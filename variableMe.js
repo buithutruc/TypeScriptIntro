126d0dd94912b1de631bdb101b286f10e48c7c62
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-01-24 13:23:19 -0500

    updated js files written from ts

--- variableMe.js
+++ variableMe.js
@@ -1,2 +1,16 @@
-var greetings = "Hello Truc";
+"use strict";
+exports.__esModule = true;
+var greetings = "Hello Truc"; //there's no error as in the video instruction, maybe the new version fixed that issue
+greetings.toLowerCase();
 console.log(greetings);
+//functions
+var getHello = function (s) {
+    return "";
+};
+var heros = ["thor", "spiderman", "ironman"];
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+function consoleError(errMsg) {
+    console.log(errMsg);
+}
