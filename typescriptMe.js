013736b913498f00fd622c4ca5b8177c762d480d
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-01-24 13:33:51 -0500

    practicing objects in ts

--- typescriptMe.js
+++ typescriptMe.js
@@ -1,5 +1,6 @@
 "use strict";
 exports.__esModule = true;
+//variables
 var greetings = "Hello Truc"; //there's no error as in the video instruction, maybe the new version fixed that issue
 greetings.toLowerCase();
 console.log(greetings);
@@ -16,4 +17,18 @@ function consoleError(errMsg) {
 }
 function handleError(errMsg) {
     throw new Error(errMsg);
+}
+//objects
+var User = {
+    name: "truc",
+    email: "truc@gmail.com",
+    isActive: true
+};
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+var newUser = { name: "truc", isPaid: false, email: "truc@gmail.com" };
+createUser(newUser);
+function createCourse() {
+    return { name: "reactjs", price: 399 };
 }
--- typescriptMe.ts
+++ typescriptMe.ts
@@ -4,8 +4,6 @@ let greetings: string = "Hello Truc"; //there's no error as in the video instruc
 greetings.toLowerCase();
 console.log(greetings);
 
-export {};
-
 //functions
 const getHello = (s: string): string => {
   return "";
@@ -24,3 +22,22 @@ function consoleError(errMsg: string): void {
 function handleError(errMsg: string): never {
   throw new Error(errMsg);
 }
+
+//objects
+const User = {
+  name: "truc",
+  email: "truc@gmail.com",
+  isActive: true,
+};
+
+function createUser({ name: string, isPaid: boolean }) {}
+
+let newUser = { name: "truc", isPaid: false, email: "truc@gmail.com" };
+
+createUser(newUser);
+
+function createCourse(): { name: string; price: number } {
+  return { name: "reactjs", price: 399 };
+}
+
+export {};
