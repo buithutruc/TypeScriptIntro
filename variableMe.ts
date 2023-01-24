611ff7450a5e80df2c5a5c58f0075dce75496c9c
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-01-24 13:22:58 -0500

    practicing functions in ts

--- variableMe.ts
+++ variableMe.ts
@@ -4,3 +4,22 @@ greetings.toLowerCase();
 console.log(greetings);
 
 export {};
+
+//functions
+const getHello = (s: string): string => {
+  return "";
+};
+
+const heros = ["thor", "spiderman", "ironman"];
+
+heros.map((hero): string => {
+  return `hero is ${hero}`;
+});
+
+function consoleError(errMsg: string): void {
+  console.log(errMsg);
+}
+
+function handleError(errMsg: string): never {
+  throw new Error(errMsg);
+}
