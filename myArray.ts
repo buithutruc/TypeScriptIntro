c9b2cec2352c35ca9c3d0a4e9830de4c76e2c62c
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-01-28 18:09:13 -0500

    practicing arrays in ts

---
+++ myArray.ts
@@ -0,0 +1,15 @@
+const superHeros: string[] = [];
+// const heroPower: number[] = []
+const heroPower: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+const mlModels: number[][] = [[255, 255, 255], []];
+
+superHeros.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "", isActive: true });
