903fca425c0a4c78fbac2a07479ef8f87ed5c2f2
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-02-03 16:54:19 -0500

    practicing interface in ts

---
+++ myInterface.ts
@@ -0,0 +1,23 @@
+interface User {
+  readonly dbId: number;
+  email: string;
+  userId: number;
+  googleId?: string;
+  //   startTrail: () => string
+  startTrail(): string;
+  getCoupon(couponName: string, value: number): number;
+}
+
+const truc: User = {
+  dbId: 22,
+  email: "h@h.com",
+  userId: 2211,
+  startTrail: () => {
+    return "trail started";
+  },
+  getCoupon: (name: "truc", off: 10) => {
+    return 10;
+  },
+};
+
+truc.email = "h@hc.com";
