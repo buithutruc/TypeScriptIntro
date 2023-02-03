9fe690f95cda897d55d3cb8a298338acf87ecbf3
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-02-03 16:57:39 -0500

    practiced interface in ts

--- myInterface.ts
+++ myInterface.ts
@@ -8,10 +8,20 @@ interface User {
   getCoupon(couponName: string, value: number): number;
 }
 
-const truc: User = {
+interface User {
+  githubToken: string;
+}
+
+interface Admin extends User {
+  role: "admin" | "ta" | "learner";
+}
+
+const truc: Admin = {
   dbId: 22,
   email: "h@h.com",
   userId: 2211,
+  role: "admin"
+  githubToken: "github",
   startTrail: () => {
     return "trail started";
   },
